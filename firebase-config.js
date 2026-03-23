a8b8122236c0eacbefce2c6303725f7643c301be
Hassan <hazytarzan12@gmail.com>
2026-03-24 00:54:48 +0100

    chore: update Firebase configuration to dedicated barbershop database

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,21 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getDatabase, ref, set, get, onValue } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+import { getDatabase, ref, set, get, onValue } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-database.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-analytics.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAH6GOOb20QOgMJ3Uv_S5Ppj-1u2Xe-UsI",
-    authDomain: "bellacucina-8d2f6.firebaseapp.com",
-    databaseURL: "https://bellacucina-8d2f6-default-rtdb.firebaseio.com",
-    projectId: "bellacucina-8d2f6",
-    storageBucket: "bellacucina-8d2f6.firebasestorage.app",
-    messagingSenderId: "817680498309",
-    appId: "1:817680498309:web:15049eeaa6ba5f46b8b3ec",
-    measurementId: "G-BH1Q0C4RLT"
+    apiKey: "AIzaSyDnKXal34hBwcOP2WGh2z1CC2_q1lnVSX0",
+    authDomain: "barbershop-booking-9937c.firebaseapp.com",
+    databaseURL: "https://barbershop-booking-9937c-default-rtdb.firebaseio.com",
+    projectId: "barbershop-booking-9937c",
+    storageBucket: "barbershop-booking-9937c.firebasestorage.app",
+    messagingSenderId: "760947042341",
+    appId: "1:760947042341:web:2cd8473757fb82b4c877cc",
+    measurementId: "G-G46D2LEWM0"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const db = getDatabase(app);
 
 export { db, ref, set, get, onValue };
